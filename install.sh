abcfa71fafe9540f6b4aef4049563adb869bc557
panachainy <panachai.ny@gmail.com>
2021-02-17 00:16:50 +0700

    [install] update

--- install.sh
+++ install.sh
@@ -6,21 +6,27 @@
 
 # == general command =======================================================================================================
 
-sed ':a;N;$!ba;s/\n//g' # for remove new line
+remove_new_line(){
+    _remove_new_line_tmp="`echo $1 | sed ':a;N;$!ba;s/\n//g'`"
+}
+
+write_file_from_blob_type(){
+    remove_new_line $1
+    echo $_remove_new_line_tmp | json content | base64 -d > $2
+}
 
 # == tree ==================================================================================================================
 
-baseUrl='https://api.github.com/repos/panachainy/github-auto-release/git/trees/develop'
-jsonData=`curl -H "Accept: application/vnd.github.v3+json" $baseUrl`
+_base_url='https://api.github.com/repos/panachainy/github-auto-release/git/trees/develop'
+_json_data=`curl -H "Accept: application/vnd.github.v3+json" $_base_url`
 
 # if tree use this
-echo $jsonData | json tree | json 0 | json type
-echo $jsonData | json tree | json 0 | json path
-echo $jsonData | json tree | json 0 | json url
+echo $_json_data | json tree | json 0 | json type
+echo $_json_data | json tree | json 0 | json path
+echo $_json_data | json tree | json 0 | json url
 
 # == content ===============================================================================================================
 
-have_content=`curl https://api.github.com/repos/panachainy/github-auto-release/git/blobs/eb079ffdc682e0868619fda16817bad33afcbd9e`
+_have_content=`curl https://api.github.com/repos/panachainy/github-auto-release/git/blobs/eb079ffdc682e0868619fda16817bad33afcbd9e`
 
-# if blob use this
-echo $have_content | sed ':a;N;$!ba;s/\n//g' | json content | base64 -d > readme.txt
+write_file_from_blob_type $_have_content "c.txt"
