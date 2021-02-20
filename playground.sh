1c1c88de16197751ced9c3d6c968983df14ddfbc
Panachai <panachai.ny@gmail.com>
2021-02-20 21:12:13 +0700

    Update playground.sh

--- playground.sh
+++ playground.sh
@@ -1,48 +1,20 @@
-get_data() {
-    echo $(curl -H "Accept: application/vnd.github.v3+json" $1)
+fuction_get_string() {
+    echo "$1 asdasd;lqwkdjqwpodajksdasd"
 }
 
-remove_new_line() {
-    echo $(echo $1 | sed ':a;N;$!ba;s/\n//g')
+function_test_main() {
+    result_global=$(fuction_get_string "asd")
+
+    echo "================================="
+    echo $result_global
+    echo "================================="
+
+    local result_local=$(echo $result_global)
+    echo "================================="
+    echo $result_global
+    # FIXME: can't assign result to local variable.
+    echo $result_local
+    echo "================================="
 }
 
-write_file_from_blob_type() {
-    removed_line_data=$(remove_new_line "$1")
-    echo $removed_line_data | json content | base64 -d >$2
-}
-
-# url / type / path
-directory_process() {
-    current_json_data=$(get_data "$1")
-    removed_line_data=$(remove_new_line "$current_json_data")
-
-    if [ "$2" = "tree" ]; then
-
-        types=$(echo $removed_line_data | json tree | json -a type)
-        paths=$(echo $removed_line_data | json tree | json -a path)
-        urls=$(echo $removed_line_data | json tree | json -a url)
-
-        current_remove_new_line=$removed_line_data
-
-        count=0
-        for i in $types; do
-            current_object=$(echo $current_remove_new_line | json tree | json $count)
-
-            url=$(echo $current_object | json url)
-            type=$(echo $current_object | json type)
-            path=$(echo $current_object | json path)
-
-            echo "========="
-            echo $type
-            echo $path
-            echo $url
-            echo "========="
-
-            count=$(($count + 1))
-        done
-    elif [ "$2" = "blob" ]; then
-        write_file_from_blob_type "$1" "$3"
-    fi
-}
-
-directory_process "https://api.github.com/repos/panachainy/github-auto-release/git/trees/develop" "tree"
+function_test_main
