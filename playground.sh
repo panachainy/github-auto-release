4507a4106c530c5c627e902bb7c31e9f93457800
Panachai <panachai.ny@gmail.com>
2021-02-27 21:43:13 +0700

    Update playground.sh

--- playground.sh
+++ playground.sh
@@ -1,27 +1,10 @@
-fuction_get_string() {
-    echo "parameter: $1 test echo with parameter"
-}
-
-function_test_main() {
-    result_global=$(fuction_get_string "parameter1")
-
-    echo "=1================================"
-    echo $result_global
-    echo "================================="
-
-    local result_local
-    result_local=$(fuction_get_string "parameter2")
-    echo "=2================================"
-    # FIXME: can't assign result to local variable.
-    echo $result_local
-    echo "================================="
-}
-
 # function_test_main
 
-result_fuction_get_string=$(fuction_get_string "parameter2")
-if [ "$result_fuction_get_string" = "parameter: parameter2 test echo with parameter" ]; then
-    echo "success"
-else
-    echo "fail: $result_fuction_get_string"
-fi
+# result_fuction_get_string=$(fuction_get_string "parameter2")
+# if [ "$result_fuction_get_string" = "parameter: parameter2 test echo with parameter" ]; then
+#     echo "success"
+# else
+#     echo "fail: $result_fuction_get_string"
+# fi
+
+# https://api.github.com/repos/panachainy/github-auto-release/git/trees/develop
